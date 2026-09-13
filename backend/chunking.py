25d639260175a828273c32c3f5892e49430b9b2d
Jukanti Abhinaya <j.abhinaya12@gmail.com>
2026-09-13 09:19:50 +0530

    Modify chunk_text to process a list of pages
    Refactor chunk_text function to handle multiple pages and return structured chunks.

--- backend/chunking.py
+++ backend/chunking.py
@@ -1,12 +1,21 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-def chunk_text(text):
 
+def chunk_text(pages):
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=500,
         chunk_overlap=100
     )
 
-    chunks = splitter.split_text(text)
+    chunks = []
+
+    for page in pages:
+        page_chunks = splitter.split_text(page["text"])
+
+        for chunk in page_chunks:
+            chunks.append({
+                "text": chunk,
+                "page": page["page"]
+            })
 
     return chunks
